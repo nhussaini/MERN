1f5a611da53fe187d9847f25bfc4dc217c7580b2
unknown <nhussaini@luc.edu>
2020-04-01 12:16:23 -0700

    another commit

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,75 +1,104 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
-import {VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH} from '../../shared/util/Validators';
-
-
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from '../../shared/util/Validators';
+import { useForm } from '../../shared/hooks/form-hook';
+import './PlaceForm.css';
 
 const DUMMY_PLACES = [
-    {
-        id:'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famouse sky scrapers in the world',
-        imageUrl:'https://images.unsplash.com/photo-1502104034360-73176bb1e92e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-        address: '20 W 34th st, New York, NY ',
-        location: {
-            lat:40.7484405,
-            lng:-73.9877095
-        },
-        creator:'u1'
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
     },
-    {
-        id:'p2',
-        title: 'Empire State Building',
-        description: 'One of the most famouse sky scrapers in the world',
-        imageUrl:'https://images.unsplash.com/photo-1502104034360-73176bb1e92e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-        address: '20 W 34th st, New York, NY ',
-        location: {
-            lat:40.7484405,
-            lng:-73.9877095
-        },
-        creator:'u2'
-    }
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u2'
+  }
 ];
 
-const UpdatePlace =()=>{
-    const placeId = useParams().placeId;
-    const identifiedPlace = DUMMY_PLACES.find(p=>p.id=== placeId);
-    if(!identifiedPlace){
-        return (
-            <div className="center">
-                <h2>Could not find place</h2>
-            </div>
-        );
-    }
-    return <form>
-        <Input
-         id="title" 
-         element="input" 
-         type="text" 
-         lable="Title" 
-         validators={[VALIDATOR_REQUIRE()]}
-         errorText="Please enter a valid title"
-         onInput={()=>{}}
-         value={identifiedPlace.title}
-         valid={true}
-         />
-         <Input
-         id="description" 
-         element="textarea" 
-         lable="description" 
-         validators={[VALIDATOR_MINLENGTH(5)]}
-         errorText="Please enter a valid description (min. 5 characters)."
-         onInput={()=>{}}
-         value={identifiedPlace.description}
-         valid={true}
-         />
+const UpdatePlace = () => {
+  const placeId = useParams().placeId;
 
-         <Button type="submit" disable={true}>UPDATE PLACE</Button>
+  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
 
-    </form>;
-}
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: identifiedPlace.title,
+        isValid: true
+      },
+      description: {
+        value: identifiedPlace.description,
+        isValid: true
+      }
+    },
+    true
+  );
+
+  const placeUpdateSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <h2>Could not find place!</h2>
+      </div>
+    );
+  }
+
+  return (
+    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={inputHandler}
+        initialValue={formState.inputs.title.value}
+        initialValid={formState.inputs.title.isValid}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min. 5 characters)."
+        onInput={inputHandler}
+        initialValue={formState.inputs.description.value}
+        initialValid={formState.inputs.description.isValid}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
+};
 
 export default UpdatePlace;
